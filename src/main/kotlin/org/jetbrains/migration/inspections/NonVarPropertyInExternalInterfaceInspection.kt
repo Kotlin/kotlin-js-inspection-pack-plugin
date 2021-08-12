4e4fe25be652b575e4a1d148e6c261f74b3150e2
Viktor.Noskin <viktor.noskin@jetbrains.com>
2021-08-12 16:01:02 +0300

    fix: NonVarPropertyInExternalInterfaceInspection now inspect only external interfaces that extended from `react.State` or `react.Props`

--- src/main/kotlin/org/jetbrains/migration/inspections/NonVarPropertyInExternalInterfaceInspection.kt
+++ src/main/kotlin/org/jetbrains/migration/inspections/NonVarPropertyInExternalInterfaceInspection.kt
@@ -7,15 +7,19 @@ package org.jetbrains.migration.inspections
 import com.intellij.codeInspection.IntentionWrapper
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
 import org.jetbrains.kotlin.idea.project.platform
 import org.jetbrains.kotlin.idea.quickfix.ChangeVariableMutabilityFix
+import org.jetbrains.kotlin.idea.search.usagesSearch.descriptor
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.platform.js.isJs
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.declarationVisitor
 import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
+import org.jetbrains.migration.react.implementsRProps
+import org.jetbrains.migration.react.implementsRState
 
 class NonVarPropertyInExternalInterfaceInspection : AbstractKotlinInspection() {
     override fun buildVisitor(
@@ -25,7 +29,9 @@ class NonVarPropertyInExternalInterfaceInspection : AbstractKotlinInspection() {
         if (!declaration.platform.isJs()) return@declarationVisitor
         val parent = declaration.containingClassOrObject as? KtClass ?: return@declarationVisitor
         val property = declaration as? KtProperty ?: return@declarationVisitor
-        if (parent.isInterface() && parent.hasModifier(KtTokens.EXTERNAL_KEYWORD) && !property.isVar) {
+        val parentClassDescriptor = parent.descriptor as? ClassDescriptor ?: return@declarationVisitor
+        val parentImplementsReactStateOrProps = parentClassDescriptor.implementsRProps || parentClassDescriptor.implementsRState
+        if (parent.isInterface() && parent.hasModifier(KtTokens.EXTERNAL_KEYWORD) && parentImplementsReactStateOrProps && !property.isVar) {
             holder.registerProblem(
                 property.valOrVarKeyword,
                 "Property in external interface should be var",
