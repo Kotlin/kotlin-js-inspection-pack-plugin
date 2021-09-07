211a8fed475c2ac8692260771b37909ddc23e760
Viktor.Noskin <viktor.noskin@jetbrains.com>
2021-09-07 09:55:41 +0300

    refactor: use classOrObjectVisitor method for NonExternalRPropsInspection

--- src/main/kotlin/org/jetbrains/migration/inspections/NonExternalRPropsInspection.kt
+++ src/main/kotlin/org/jetbrains/migration/inspections/NonExternalRPropsInspection.kt
@@ -14,38 +14,29 @@ import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
 import org.jetbrains.kotlin.idea.project.platform
 import org.jetbrains.kotlin.idea.search.usagesSearch.descriptor
 import org.jetbrains.kotlin.platform.js.isJs
-import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtObjectDeclaration
+import org.jetbrains.kotlin.psi.classOrObjectVisitor
 
 class NonExternalRPropsInspection : AbstractKotlinInspection() {
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
-        return NonExternalRPropsVisitor(holder)
-    }
-}
 
-class NonExternalRPropsVisitor(
-    private val holder: ProblemsHolder,
-) : KtVisitorVoid() {
-    override fun visitKtFile(file: KtFile) {
-        if(file.platform.isJs()) {
-            super.visitKtFile(file)
-        }
-    }
-
-    override fun visitClassOrObject(classOrObject: KtClassOrObject) {
-        if (classOrObject.containingKtFile.platform.isJs()) {
-            val classDescriptor = classOrObject.descriptor as? ClassDescriptor ?: return
+    override fun buildVisitor(
+        holder: ProblemsHolder, isOnTheFly: Boolean
+    ): PsiElementVisitor = classOrObjectVisitor { classOrObject ->
+        if (classOrObject.platform.isJs()) {
+            val classDescriptor = classOrObject.descriptor as? ClassDescriptor ?: return@classOrObjectVisitor
             if (classDescriptor.implementsRProps || classDescriptor.implementsRState) {
                 if(classOrObject is KtClass) {
                     if (classOrObject.isInterface() && !classDescriptor.isExternal) {
-                        val nameIdentifier = classOrObject.nameIdentifier ?: return
+                        val nameIdentifier = classOrObject.nameIdentifier ?: return@classOrObjectVisitor
                         holder.registerProblem(nameIdentifier, "Interface should be external", AddExternalQuickFix)
                     } else {
-                        val classKeyword = classOrObject.getClassKeyword() ?: return
+                        val classKeyword = classOrObject.getClassKeyword() ?: return@classOrObjectVisitor
                         holder.registerProblem(classKeyword, "Class should be external interface")
                     }
                 }
                 if(classOrObject is KtObjectDeclaration){
-                    val objectKeyword = classOrObject.getObjectKeyword() ?: return
+                    val objectKeyword = classOrObject.getObjectKeyword() ?: return@classOrObjectVisitor
                     holder.registerProblem(objectKeyword, "Object should be external interface")
                 }
             }
